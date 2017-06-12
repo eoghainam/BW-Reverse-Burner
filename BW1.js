9751c60df1dc22b8bb6361fed283c3283c55ae30
eoghainam <eoghainam@users.noreply.github.com>
2017-06-12 18:10:14 +0100

    added image

--- BW1.js
+++ BW1.js
@@ -1,3 +1,3 @@
-var bw = [{name:"born slave", leadsTo:["Jester"]}];
+var bw = [{name:"born slave", level:1, leadsTo:["Jester"]}];
 
-document.write(bw[0].name + "</BR>" + bw[0].leadsTo[0]);
+document.write(bw[0].name + "</BR>" + bw[0].leadsTo[0] + "</BR>");
