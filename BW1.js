356ad4540dcbfb82cd4ae56a08a1675a2e7e23f4
eoghainam <eoghainam@users.noreply.github.com>
2017-06-13 23:15:01 +0100

    Seperated lifepaths lists. Started with Humans

--- BW1.js
+++ BW1.js
@@ -1,3 +1,3 @@
-var bw = {human:[[{name:"born slave", level:1, leadsTo:["Jester"]}]]};
 
-document.write(bw.human[0][0].name + "</BR>" + bw.human[0][0].leadsTo[0] + "</BR>");
+
+document.write(human[0].leadsTo[0]);
